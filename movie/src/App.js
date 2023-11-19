3d5e887c43bce7ccfd1f00f10f9adb908b036594
LoreBarrera <karenlbarrera@gmail.com>
2023-11-19 00:45:24 -0500

    Cuadricula en grid

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import MovieList from './components/MovieList/MovieList';
+import React, { useState, useEffect } from 'react';
+import MovieGrid from '../src/components/MovieGrid/MoviGrid'
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const apiKey = 'c122c849039f792fd480b3e7aef4721f';
+        const response = await fetch(
+          `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&page=${currentPage}`
+        );
+        const data = await response.json();
+        setMovies(data.results);
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
+    };
+
+    fetchMovies();
+  }, [currentPage]);
+
   return (
-    <div className="App">
-      <MovieList />
+    <div>
+      <h1>Movies</h1>
+      <MovieGrid movies={movies} />
+      <button onClick={() => setCurrentPage((prev) => prev + 1)}>Next Page</button>
     </div>
   );
 }
