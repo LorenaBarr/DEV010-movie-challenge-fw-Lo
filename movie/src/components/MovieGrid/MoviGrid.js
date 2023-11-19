cc1812b50fed56f6ac0c3009540942330f43e6e5
LoreBarrera <karenlbarrera@gmail.com>
2023-11-19 00:36:29 -0500

    Componentes, primera lista de peliculas

---
+++ movie/src/components/MovieGrid/MoviGrid.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MovieGrid.css';
+
+const MovieGrid = () => {
+  return (
+    <div className="movie-grid">
+      {/* Contenido de la cuadrícula de películas */}
+    </div>
+  );
+};
+
+export default MovieGrid;
