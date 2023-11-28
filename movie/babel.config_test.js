2de33859fd26cb4f096cd1a552f2a692aaea5d79
LoreBarrera <karenlbarrera@gmail.com>
2023-11-28 18:36:45 -0500

    configuraciones de los test

---
+++ movie/babel.config_test.js
@@ -0,0 +1,25 @@
+// module.exports = {
+//     presets: ['@babel/preset-env', '@babel/preset-react'],
+//   };
+
+
+// module.exports = {
+//   presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
+// };
+
+// module.exports = async () => ({
+//     presets: ['@babel/preset-env', '@babel/preset-react'],
+//   });
+
+// module.exports = function(api) {
+//     api.cache(true);
+  
+//     const presets = [
+//       '@babel/preset-env',
+//       '@babel/preset-react',
+//     ];
+  
+//     return {
+//       presets,
+//     };
+//   };
