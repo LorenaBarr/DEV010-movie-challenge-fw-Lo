ea224b1e64180a8481e613dec17977b96f8ba7e1
LoreBarrera <karenlbarrera@gmail.com>
2023-11-29 08:37:47 -0500

    Cambios en el componenten de App.jsx optimizando la funcion

--- movie/src/App.jsx
+++ movie/src/App.jsx
@@ -2,8 +2,21 @@ import { useState, useEffect } from 'react';
 import MovieGrid from './components/MovieGrid/MovieGrid';
 import Pagination from './components/MoviePages/Pagination';
 import MovieSearch from './components/MovieSearch/MovieSearch';
-// import 'App.css';
 import axios from 'axios';
+import './App.css';
+
+const fetchMovies = async (options ) => {
+  try {
+    const apiKey = 'c122c849039f792fd480b3e7aef4721f';
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=16&page=${options.page}`
+    );
+    options.onSuccess(response.data)
+  
+  } catch (error) {
+    console.error('Error fetching movies:', error);
+  }
+};
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -11,20 +24,15 @@ function App() {
   const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
-    const fetchMovies = async () => {
-      try {
-        const apiKey = 'c122c849039f792fd480b3e7aef4721f';
-        const response = await axios.get(
-          `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=16&page=${currentPage}`
-        );
-        setMovies(response.data.results);
-        setTotalPages(response.data.total_pages);
-      } catch (error) {
-        console.error('Error fetching movies:', error);
-      }
-    };
+    const options  = {
+    page: currentPage, 
+    onSuccess: (data) =>{
+      setMovies(data.results);
+    setTotalPages(data.total_pages);
+    }
+    }
 
-    fetchMovies();
+    fetchMovies(options);
   }, [currentPage]);
 
   const handlePageChange = (page) => {
@@ -40,5 +48,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
